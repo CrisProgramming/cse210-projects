76b9a84f634de58d8d279a9bcdc2df15643da274
crisprogramming <olg24001@byui.edu>
2025-04-15 19:53:11 -0700

    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create shape instances
+        Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Blue", 4, 6);
+        Circle circle = new Circle("Green", 3);
+
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        // Iterate and display their colors and areas
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
